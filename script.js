bf6e6c78dbd75b0eed801f256b58b96c14897b34
TarsisViana <159029529+TarsisViana@users.noreply.github.com>
2024-03-08 15:01:47 +0000

    Link the buttons to the js file
    Each category of button is linked to the js file and alters the display
    in the correct way, no operations linked yet

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-let num1 = 1;
-let num2 = 2;
+let num1 = 0;
+let num2 = 0;
 let operator;
+let result = 0;
 
-const btn1 = document.querySelector('.minus');
+const display = document.querySelector('.display');
+const numberBtn = document.querySelectorAll('.number');
+const operatorBtn = document.querySelectorAll('.operator');
+const eraseBtn = document.querySelectorAll('.erase');
+const operateBtn = document.querySelector('#operate');
 
 //object containing all the operation functions
-
 let operations = {
   subtract: (num1,num2) => num1 - num2,
   multiply: (num1, num2) => num1 * num2,
@@ -14,9 +18,45 @@ let operations = {
 };
 
 //function that calls the corresponding operation from the object id
-
 function operate(num1,num2,operator){
   return operations[operator.id](num1,num2);
 };    
 
-console.log(operate(num1,num2,btn1));
+//links the numbers buttons to the needed functions
+numberBtn.forEach((btn)=> {
+  btn.addEventListener('click',() => {
+    if (display.textContent == 0) display.textContent = '';
+    display.textContent += btn.textContent;
+    
+  });
+});
+
+//links the operators numbers to the needed functions
+operatorBtn.forEach((btn) => {
+  btn.addEventListener('click',() => {
+    if (display.textContent == 0) return;
+    display.textContent += btn.textContent;
+  });
+});
+
+//links the erase buttons to the needed functions
+eraseBtn.forEach((btn) => {
+  btn.addEventListener('click',() => {
+    if (display.textContent == 0) return;
+    if (btn.id == 'clear') {
+      display.textContent = 0;
+      return;
+    }
+    if(btn.id == 'del'){
+      display.textContent = display.textContent.slice(0,-1);
+      if (display.textContent == '') display.textContent = 0;
+      return;
+    }
+  });
+});
+
+//liks the equal buttons to start the operations
+operateBtn.addEventListener('click',() => {
+  result = operate(num1, num2, operator);
+  display.textContent = result; 
+});
