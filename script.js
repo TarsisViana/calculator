2c87d033691c3f8ff44835d189ab6b599472705d
TarsisViana <159029529+TarsisViana@users.noreply.github.com>
2024-03-09 17:50:56 +0000

    Make the operands input one at a time
    Reworked the previous version where the operands were read from the full
    operation string, now it's simplified and the display only shows the current
    input value or result

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let num1 = 0;
-let num2 = 0;
+let num1 = null;
+let num2 = null;
 let operator;
-let result = 0;
+let result = null;
+let input = '';
 let tempNumber = '';
-let memory = '';
 let equalTest = false;
 
 const display = document.querySelector('.display');
@@ -13,35 +13,41 @@ const eraseBtn = document.querySelectorAll('.erase');
 const equalBtn = document.querySelector('#equal');
 
 //object containing all the operation functions
-let operations = {
+const operations = {
   subtract: (num1,num2) => num1 - num2,
   multiply: (num1, num2) => num1 * num2,
-  divide: (num1, num2) => num1 / num2,
-  add: (num1,num2) => num1 + num2,  
+  divide: (num1, num2) => {
+    if(num2 == 0) return 'nope'
+    else return num1 / num2
+  },
+  add: (num1,num2) => num1 + num2,
 };
 
-//function that reads the string for the numbers and calls the opereration
-function operate(text,operator){
-  if(text.search(/[\+\-*/]/) == -1) return text;
-  num1 = parseInt(text.slice(0, text.search(/[\+\-*/]/)));
-  num2 = parseInt(text.slice(parseInt(text.search(/[\+\-*/]/))+1));
-  if (isNaN(num1) || isNaN(num2)) return text;
-  equalTest = true;                       
+
+function operate(num1, num2, operator){  
+  if (isNaN(num1) || isNaN(num2)) return display.textContent;
+  if (operator == undefined) return display.textContent;                    
   return operations[operator.id](num1,num2);
 };    
 
-//links the numbers buttons and updates display
+
+//links the numbers buttons, updates display, adds the input
 numberBtn.forEach((btn)=> {
   btn.addEventListener('click',() => {
     updateDisplay(display.textContent,btn);
+    input += btn.textContent;
   });
 });
+
+
 
 function updateDisplay(text,btn) {
   if(btn !== undefined){
     if(btn.className.includes('number')){
       if (text == 0) display.textContent = '';
       if (equalTest) display.textContent = '';  //resets if number pressed after =
+      if (operator != undefined) display.textContent = '';
+      if (input == '') display.textContent = '';
       display.textContent += btn.textContent;
 
     }else if(btn.className.includes('operator')){
@@ -56,30 +62,34 @@ function updateDisplay(text,btn) {
 };
 
 //links the operators buttons
-//check the display string if its a multiple operation
+//check the operator if its a multiple operation
 //if true do the first operation and num1 receives the
 //result for the next operation. (exercise demand)
 operatorBtn.forEach((btn) => {
   btn.addEventListener('click',() => {
-    checkMultipleOperations(display.textContent,btn,operator)
-    updateDisplay(display.textContent,btn,operator);
-    operator = btn;                           
+    if(operator != undefined){
+      getOperands(input);
+      result = operate(num1,num2,operator);
+      updateDisplay(result);
+      operator.classList.toggle('pressed');
+    }
+    else getOperands(input);
+    operator = btn;
+    btn.classList.toggle('pressed');                           
   });
 });
 
-function checkMultipleOperations(displayText,btn,oldOperator){
-  
-  if(displayText.match(/[\+\-*/]/)){
-    let length = parseInt(displayText.length)-1;
-    let opPosition = displayText.search(/[\+\-*/]/);
-    if(opPosition != length && opPosition != '-1'){
-      tempNumber = operate(display.textContent,oldOperator);
-      num1 = tempNumber;
-      updateDisplay(num1);
-    }
+function getOperands(number){
+  if (operator != undefined){
+    num2 = parseInt(number);
+    input = '';
+  }else {
+    num1 = parseInt(number);
+    input = '';
   }
-  
+  console.log(num1,num2)
 }
+
 
 
 //allows user to change the operator if a mistake was made
@@ -94,8 +104,10 @@ function changeOperator(string) {
 
 //liks the equal button to start the operations
 equalBtn.addEventListener('click',() => {
-  result = operate(display.textContent, operator);
-  display.textContent = result; 
+  getOperands(input);
+  display.textContent = operate(num1, num2, operator);
+  num1 = parseInt(display.textContent);
+  console.log(num1, operator)
 });
 
 
@@ -106,8 +118,8 @@ eraseBtn.forEach((btn) => {
     if (display.textContent == 0) return;
     if (btn.id == 'clear') {
       display.textContent = 0;
-      num1 = num2 = result = 0;
-      tempNumber = '';
+      num1 = num2 = result = operator = null;
+      input = '';
       return;
     }
     if(btn.id == 'del'){
